3f6b7210609f2ab0214f90ed525e29db651a897f
Zyad Mohamed <fut0r@infiniware.bid>
2026-04-23 19:01:20 +0300

    Delete react version directory

--- react version/app/(tabs)/quran.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { QuranBrowser } from '@components/QuranBrowser';
-
-export default function QuranScreen() {
-  return <QuranBrowser />;
-}
--- react version/index.js
+++
@@ -1,4 +0,0 @@
-import App from './app/_layout';
-import { AppRegistry } from 'react-native';
-
-AppRegistry.registerComponent('main', () => App);
